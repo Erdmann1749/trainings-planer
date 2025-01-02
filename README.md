e7e43444a1ea1a8d5e8f9efb6a8310c2815cfbe9
Felix Ott <ott.felix@bcg.com>
2025-01-02 18:04:08 +0100

    added first version of streamlit input and visualisation

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # trainings-planer
+
+## Set up
+
+Create virtual env.
+
+On macOS/Linux:
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+On Windows:
+```bash
+python -m venv venv
+venv\Scripts\activate
+```
+
+All required packages for this project are listed in the requirements.txt file.
+```bash
+pip install -r requirements.txt
+```
+
+## run
+```bash
+streamit run planer.py
+```
+Stop with Ctrl+C in terminal.
+
+## updating dependencies:
+```bash
+pip install new-package
+pip freeze > requirements.txt
+```
+
+## end
+Deactivate the environment:
+```bash
+deactivate
+```
+
+
--- planer.py
+++ planer.py
@@ -1,9 +1,51 @@
-def ask_for_input():
-    # function asks for user input
-    name = input("Name des Trainers: ")
-    time = input("Zeit der Trainingsstunde: ")
-    day = input("Tag der Trainingsstunde: ")
-    return f"{name} gibt am {day} um {time} Uhr eine Trainingsstunde."
+import streamlit as st
+import json
+from pathlib import Path
 
-if __name__ == "__main__":
-    print(ask_for_input())
+
+def store_input(data, coach, day, hour, clients):
+    if coach in data:
+        if day in data[coach]:
+            data[coach][day][str(hour)] = clients.split(",")
+        else:
+            data[coach][day] = {str(hour): clients.split(",")}
+    else:
+        data[coach] = {day: {str(hour): clients.split(",")}}
+    with open("plan.json", "w") as f:
+        json.dump(data, f)
+
+def load_data():
+    # if filename not exists
+    file_path = Path("plan.json")
+    if file_path.exists():
+        with open("plan.json", "r") as f:
+            data = json.load(f)
+    else:
+        data = {}
+    return data
+
+# App title
+st.title("Coaching Session Input Form")
+
+# Input form
+with st.form("coaching_form"):
+    # Input fields
+    coach_name = st.text_input("Coach Name")
+    day_in_week = st.selectbox("Day in Week", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+    client_names = st.text_area("Client Names (separated by commas)")
+    hour_in_day = st.time_input("Hour in Day")
+
+    # Submit button
+    data = load_data()
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.success("Session submitted successfully!")
+        store_input(data, coach_name, day_in_week, hour_in_day, client_names)
+
+    # Display the input values
+    data = load_data()
+    for coach, days in data.items():
+        for day, details in days.items():
+            st.write(f"### {coach} - {day}")
+            for hour_in_day, client_names in details.items():
+                st.write(f"- Hour: {hour_in_day} - Clients: {', '.join(client_names)}")
