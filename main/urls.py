7b9d9d3633f33fb871564e5d05a8b39931c46351
Erdmann1749 <erdmann7@icloud.com>
2025-02-04 12:54:00 +0100

    - FE added login text fields and button - FE added heading, title and info text - BE created logout function - BE using django authenticate and login - BE display error message when wrong credentials - BE redirect to addcontacts if authentication worked
    - Warning: this is now a dead end. We need logout asap

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns  = [
-    path("", views.login, name="login"),
+    path("", views.custom_login, name="login"),
     path("add-contacts/", views.addcontacts, name="addcontacts"),
     path("create-group/", views.creategroup, name="creategroup"),
     path("group-details/", views.groupdetailspage, name="groupdetailspage"),
--- main/views.py
+++ main/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-def login(request):
+def custom_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("addcontacts")  # Wo der Nutzer nach dem Login hingeleitet wird
+        else:
+            messages.error(request, "Ungültige Anmeldeinformationen")
     return render(request, "main/login.html")
 
 def addcontacts(request):
